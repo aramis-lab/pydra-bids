7a5a32bde945c6d213524410de2c0d8ab02a3b50
Ghislain Vaillant <ghislain.vaillant@icm-institute.org>
2022-10-16 10:41:06 +0200

    Add BIDS data reader factory

--- pydra/tasks/bids/utils.py
+++ pydra/tasks/bids/utils.py
@@ -1,8 +1,9 @@
 import os
+import typing as ty
 
 import pydra
 
-__all__ = ["bids_info", "bids_data_reader", "bids_data_writer"]
+__all__ = ["bids_info", "make_bids_data_reader", "bids_data_writer"]
 
 
 @pydra.mark.task
@@ -62,8 +63,53 @@ def bids_info(in_file: os.PathLike):
     )
 
 
-def bids_data_reader():
-    pass
+def make_bids_data_reader(output_query: ty.Optional[dict] = None):
+    from pydra.engine import specs
+
+    # Query BOLD and T1w modalities by default.
+    output_query = output_query or {
+        "bold": {
+            "suffix": "bold",
+            "extension": [".nii", ".nii.gz"],
+        },
+        "T1w": {
+            "suffix": "T1w",
+            "extension": [".nii", ".nii.gz"],
+        },
+    }
+
+    # Map output query keys to output spec for the task.
+    returned = {key: ty.Iterable[specs.File] for key in list(output_query.keys())}
+
+    # Define the inner files query implementation.
+    @pydra.mark.task
+    @pydra.mark.annotate({"return": returned})
+    def query_files(
+        dataset_path: os.PathLike,
+        subject_id: ty.Union[str, ty.Iterable[str]] = "*",
+        session_id: ty.Union[str, ty.Iterable[str]] = "*",
+    ):
+        """Query files from a BIDS dataset."""
+        from os import fspath
+
+        from ancpbids import BIDSLayout
+
+        layout = BIDSLayout(ds_dir=fspath(dataset_path))
+
+        results = [
+            layout.get(
+                return_type="files",
+                subject=subject_id,
+                session=session_id,
+                **query,
+            )
+            for key, query in list(output_query.items())
+        ]
+
+        # Return tuple if more than 1 query (Pydra limitation).
+        return tuple(results) if len(results) > 1 else results[0]
+
+    return query_files
 
 
 def bids_data_writer():
