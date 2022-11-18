31c58c93f8db4cde4bd8b846c96d0dfad1cdda54
Ghislain Vaillant <ghislain.vaillant@icm-institute.org>
2022-11-18 09:33:12 +0100

    Rename bids_file argument to file_path
    Makes naming more explicit that the input is expected to be a path to a file and not a file handle.

--- pydra/tasks/bids/tests/test_utils.py
+++ pydra/tasks/bids/tests/test_utils.py
@@ -4,7 +4,7 @@ import pydra.tasks.bids.utils as utils
 def test_bfi_defaults():
     task = utils.BIDSFileInfo().to_task()
 
-    assert "bids_file" in task.input_names
+    assert "file_path" in task.input_names
     assert {"entities", "suffix", "extension"} == set(task.output_names)
 
 
